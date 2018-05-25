c63cab342cc41523c15db462e74dbf3f15c6f169
Victor Diaz de Leon <victor.deleon@mopro.com>
2018-05-25 15:07:14 -0700

    Add extra pages for routing

---
+++ src/pages/AIEditor.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AIEditor = () => (
+  <div className="page page--aieditor">
+    <h1>AIEditor</h1>
+  </div>
+);
+
+export default AIEditor;
---
+++ src/pages/Blueprint.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Blueprint = () => (
+  <div className="page page--blueprint">
+    <h1>Blueprint</h1>
+  </div>
+);
+
+export default Blueprint;
