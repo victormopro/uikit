6fc2fa130b42fc46c9ff5f3569ec8e1ffab84ac0
Jayabal <m.jayabal@cmlmediasoft.com>
2018-06-14 14:49:10 +0530

    Profile card update

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -2,71 +2,102 @@ import React from 'react';
 import Button from './Button';
 
 
-// const ProfileCard = ({img, name, profile, description}) => (
-
-//     <div className="card">
-//         <figure>
-//             <img src={img} />
-//             <figcaption>
-//                 <h3>{name}</h3>
-//                 <small>{profile}</small>
-//             </figcaption>
-//         </figure>
-//         <div className="card__content">
-//             <p>{description}</p>
-//         </div>
-//     </div>
-    
-// );
-
-// export default ProfileCard;
-
 export class ProfileCard extends React.Component{
     constructor(props) {
         super();
         this.name = props.name;
         this.img = props.img;
         this.profile = props.profile;
-        this.dummyProfile = 'Graphic eater, hamburger designer, not necessarily in that order'
+        this.about = props.about;
+        this.children = props.children;
+
+        // default values if user not provide any value
+        this.defaultProfile = 'Graphic eater, hamburger designer, not necessarily in that order';
+        this.defaultAbout = "Default text from component. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda at id rem qui, temporibus voluptatem possimus excepturi amet! Animi nostrum, voluptatibus quo a repudiandae itaque. Fugiat laudantium sint."
+        this.defaultChildren = 'Read more'
+        
     }
 
+    // user designatin 
     getProfile() {
         if(this.profile) {
             return this.profile;
         }else{
-            return this.dummyProfile;
+            return this.defaultProfile;
         }
     }
 
+    // about text content
+    getAbout() {
+        if(this.about) {
+            return this.about;
+        }else{
+            return this.defaultAbout;
+        }
+    }
+
+    // cta button
+    ctaText() {
+        if(this.children) {
+            return this.children;
+        }else{
+            return this.defaultChildren;
+        }
+    }
+
+    // social icons 
+    facebook() {
+        if(this.props.facebook) {
+            return (
+                <a href={this.props.facebook} className="social-icon__list" target="_blank">
+                    <i className="fab fa-facebook-f"></i>
+                </a>
+            );
+        }        
+    }
+    twitter() {
+        if(this.props.twitter) {
+            return(
+                <a href={this.props.twitter} className="social-icon__list" target="_blank">
+                    <i className="fab fa-twitter"></i>
+                </a>
+            )
+        }        
+    }
+    instagram() {
+        if(this.props.instagram) {
+            return (
+                <a href={this.props.instagram} className="social-icon__list" target="_blank">
+                    <i className="fab fa-instagram"></i>
+                </a>
+            )
+        }        
+    }
+    // component output
     render() {
-        let cardText = "I'm a 26-year-old art director and graphic designer based in London who loves to think and to create. I'm a calligraphy addict and an art lover. I'm a soldier and I would die for beauty. Why? Remember that the egg has a perfect shape despite coming out of an ass."
         return(
             <div className="card text-center mb-30">
-                
                 <div className="card__header">
                     <img className="card__img round" src={this.img} alt="profile" />
                 </div>
-                
-                <div className="card__content">
+                <div className="card__body">
                     <h4 className="card__title">{this.name}</h4>
                     <small className="card__subtitle">{this.getProfile()}</small>
-                    <p className="card__text">{cardText}</p>
+                    <p className="card__text">{this.getAbout()}</p>
                     <Button type="default">
-                        <span children="Read More"></span>
+                        <span>{this.ctaText()}</span>
                         <i className="fas fa-plus"></i>
                     </Button>
-                </div>
-
-                <div className="card__footer">
                     <div className="social-icon">
-                        <a href="button"><i className="fab fa-facebook-f"></i></a>
-                        <a href="button"><i className="fab fa-twitter"></i></a>
-                        <a href="button"><i className="fab fa-instagram"></i></a>
+                       {this.facebook()}
+                       {this.twitter()}
+                       {this.instagram()}
                     </div>
                 </div>
             </div>
         )
     }
+
 }
 
 export default ProfileCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -375,16 +375,37 @@ const Home = () => (
       </div>
       <div className="row mt-100">
         <div className="col-xs-4">
-          <ProfileCard img="https://placeimg.com/150/150/people" name="Bessie Lindsey" profile="UI Developer"/>
+          <ProfileCard 
+                img="https://placeimg.com/150/150/people" 
+                name="Bessie Lindsey" 
+                profile="UI Developer"
+                about="I'm a 26-year-old art director and graphic designer based in London who loves to think and to create. I'm a calligraphy addict and an art lover. I'm a soldier and I would die for beauty. Why? Remember that the egg has a perfect shape despite coming out of an ass."
+                children= 'Read more'
+                facebook="https://www.facebook.com/"
+          />
         </div>
         <div className="col-xs-4">
-          <ProfileCard img="https://placeimg.com/150/150/tech" name="Virginia Reyes" />
+          <ProfileCard 
+                img="https://placeimg.com/150/150/tech" 
+                name="Virginia Reyes"
+                profile="Graphic Designer" 
+                children= 'View more'
+                facebook="http://facebook.com"
+                twitter="https://twitter.com/"
+          />
         </div>
         <div className="col-xs-4">
-          <ProfileCard img="https://placeimg.com/150/150/nature" name="Donald Carroll" />
+          <ProfileCard 
+                img="https://placeimg.com/150/150/nature" 
+                name="Donald Carroll"
+                profile="Angular, React, JAVA developer"
+                facebook="http://facebook.com"
+                twitter="https://twitter.com/"
+                instagram="https://www.instagram.com"
+          />
         </div>
         <div className="col-xs-4">
-          <p className="text-center fw-bold">Standard Card</p>
+          {/* <p className="text-center fw-bold">Standard Card</p> */}
           <div className="card mt-30"></div>
             <CodePreview>
             {`
@@ -397,7 +418,7 @@ const Home = () => (
             </CodePreview>
         </div>
         <div className="col-xs-4">
-          <p className="text-center fw-bold">Button on bottom</p>
+          {/* <p className="text-center fw-bold">Button on bottom</p> */}
           <div className="card mt-30">
             <div className="card__footer">
               <Button size="large" type="primary" block children="Button on bottom" />
@@ -416,7 +437,7 @@ const Home = () => (
             </CodePreview>
         </div>
         <div className="col-xs-4">
-          <p className="text-center fw-bold">Button with text under</p>
+          {/* <p className="text-center fw-bold">Button with text under</p> */}
           <div className="card mt-30">
             <div className="card__footer text-center">
               <Button size="large" type="primary" children="Button with text under" />
