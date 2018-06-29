d4e5ad673f98c0a82f6e788fd122f33ff7c7c688
Jayabal <m.jayabal@cmlmediasoft.com>
2018-06-29 23:29:27 +0530

    Tabs component custom class added

--- src/components/table/InnerTabs.js
+++ src/components/table/InnerTabs.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
 import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
-
 const InnerTabs = ({tabsNav, tabsBody}) => {
   return (
-    <div className="card tabs">
-      <Tabs className="tabs__nav-in">
-        <TabList >
+    <div className="card">
+      <Tabs className="tabs tabs__nav-in">
+        <TabList className="tabs__tab-list">
             {tabsNav.map((nav) => {
-                return <Tab key={nav}>{nav}</Tab>
+                return (
+                    <Tab className="tabs__tab" 
+                        selectedClassName='tabs__tab--selected' key={nav}>{nav}</Tab>
+                )
             })}
         </TabList>
         {tabsBody.map((details) => {
-            return <TabPanel key={details}><h1>{details}</h1></TabPanel>
+            return (
+                <TabPanel className="tabs__tab-panel"
+                    selectedClassName='tabs__tab-panel--selected' key={details}><h1>{details}</h1></TabPanel>
+            )
         })}
       </Tabs>
     </div>
