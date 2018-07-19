01718865f181a71dfac6395d719faf46b7c7a118
Jayabal <m.jayabal@cmlmediasoft.com>
2018-07-19 23:15:38 +0530

    updates

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,44 +1,4 @@
 import React, { Component } from 'react';
-
-// class Accordion extends Component{
-//     constructor(props){
-//         super();
-//         this.state = {
-//             visiblity : true,
-//             title : props.title,
-//             description: props.description
-//         }
-
-//         this.visiblityToggle = this.visiblityToggle.bind(this);
-
-//     }
-    
-//     visiblityToggle() {
-//         this.setState({
-//             visiblity : !this.state.visiblity
-//         });
-//     }
-
-//     render(){
-//         return(
-//             <div className="accordion">
-//                 <div className="accordion__title">
-//                     <h4 className="">{this.state.title}</h4>
-//                     <button className="accordion__close" onClick={this.visiblityToggle} >
-//                         {this.state.visiblity ? <i className="fas fa-minus"></i> : <i className="fas fa-plus"></i>}
-//                     </button>
-//                 </div>
-//                 {
-//                     this.state.visiblity &&
-//                     <div className="accordion__description">
-//                         <p>{this.state.description}</p>
-//                     </div>
-//                 }
-//             </div>
-//         )
-//     }
-// }
-
 class Accordion extends Component{
     constructor(props){
         super();
@@ -78,6 +38,5 @@ class Accordion extends Component{
         )
     }
 }
-
 
 export default Accordion;
